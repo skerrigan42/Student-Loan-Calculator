49b89bfd0b70d4d765cc0cc113600c098506c868
skerrigan42 <91266480+skerrigan42@users.noreply.github.com>
2021-10-14 23:51:03 -0400

    Add files via upload

---
+++ src/main/java/com/example/studentloancalculator/Question.java
@@ -0,0 +1,17 @@
+package com.example.studentloancalculator;
+
+public class Question {
+    private int mTextResId;
+
+    public Question(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public int getmTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+}
